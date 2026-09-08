cc41057a48f6760ec888f31ac8867d19d7770dfb
Puja Rai <pujarai7000@gmail.com>
2026-09-07 23:40:21 -0500

    Displays each individual expense

--- expense_tracker.py
+++ expense_tracker.py
@@ -21,5 +21,10 @@ for i in range (len(Expense)):
  z=Expense[i].get("price")
  count+=z
 
+for i in range(len(Expense)):
+  f= Expense[i].get("category")
+  g=Expense[i].get("price")
+  print(f,g)
+
 print(f"The total amount spent is {count}")
        
