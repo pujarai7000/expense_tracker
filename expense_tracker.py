d8b42ee1fb2fc0e4c24dbd6e623bf62198bd6ec9
Puja Rai <pujarai7000@gmail.com>
2026-09-19 19:18:55 -0500

    Update expense tracker with menu option

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,2 +1,34 @@
+Expense =[]
 
+while True:
+       print("1. Add expense")
+       print("2. View expense")
+       print("3. View total")
+       print("4. Exit")
+       option=input("Choose option\n")
+       
+       
+       if option=="1" :
+          category=input("Enter the category.\n")
+          price=float(input("Enter the price.\n"))
+          expense={}
+          expense["category"]= category
+          expense["price"]=price
+      
+          Expense.append(expense)
+          print(expense)
+       elif option =="2":
+             for detail in Expense:
+              print(detail)
+       elif option =="3":
+           count=0
+           for i in range (len(Expense)):
+            z=Expense[i].get("price")
+            count+=z
+
+           print(f"The total amount spent is {count}")
+             
+       if option =="4" :
+                     break
+       
 
